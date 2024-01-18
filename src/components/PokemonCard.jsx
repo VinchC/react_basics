a97975ff73bac47943e14123c8ccc96fa1c792e6
Vincent C <vinch.cm@gmail.com>
2024-01-18 14:53:24 +0100

    CSS added with className

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,13 @@
+import styles from './PokemonCard.module.css';
+
 function PokemonCard() {
   return (
     <div>
-      <figure>
-        <img
+      <figure className={styles.card}>
+        <img 
           src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
           alt="Bulbasaur-picture"
+          className={styles.card_img}
         />
       </figure>
       <figcaption>Bulbasaur</figcaption>
